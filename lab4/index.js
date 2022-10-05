df0348a5a7fa8f508ceca1f43203ae306453c787
s-lando <slandolando@hotmail.com>
2022-10-05 15:11:03 -0700

    challenge patch requests

--- lab4/index.js
+++ lab4/index.js
@@ -55,4 +55,73 @@ app.get('/api/v2/unicorn/:id', (req, res) => {
     })
 })
 
+app.use(express.json())
+app.post('/api/v2/unicorn', (req, res) => {
+  // - create a new unicorn
 
+  unicornModel.create(req.body, function (err) {
+    if (err) console.log(err);
+    // saved!
+  });
+  res.json(req.body)
+})  
+
+app.patch('/api/v2/unicorn/:id', (req, res) => {
+  // - update a unicorn
+
+
+  const { _id, ...rest } = req.body;
+  unicornModel.updateOne({ _id: mongoose.Types.ObjectId(req.params.id) }, rest, function (err, res) {
+
+    if (err) console.log(err)
+    console.log(res)
+  });
+
+  res.send("Updated successfully!")
+})
+
+app.delete('/api/v2/unicorn/:id', (req, res) => {
+  // - delete a unicorn
+
+  unicornModel.deleteOne({ _id: mongoose.Types.ObjectId(req.params.id) }, function (err, result) {
+    if (err) console.log(err);
+    console.log(result);
+  });
+
+  res.send("Deleted successfully?")
+})
+
+app.patch('/api/v2/unicornNewLovesFood/:id/', (req, res) => {
+  unicornModel.updateOne({ _id: mongoose.Types.ObjectId(req.params.id) }, req.body, function (err, res) {
+    if (err) console.log(err)
+    console.log(res)
+  });
+
+  res.send("Updated successfully!")
+})
+
+app.patch('/api/v2/unicornAddLovesFood/:id/:item', (req, res) => {
+  unicornModel.updateOne({ _id: mongoose.Types.ObjectId(req.params.id) }, {
+    $push: {
+      loves: req.params.item
+    }
+  }, function (err, res) {
+
+    if (err) console.log(err)
+    console.log(res)
+  });
+  res.send("Updated successfully!")
+})
+
+app.patch('/api/v2/unicornRemoveLovesFood/:id/:item', (req, res) => {
+  unicornModel.updateOne({ _id: mongoose.Types.ObjectId(req.params.id) }, {
+    $pull: {
+      loves: req.params.item
+    }
+  }, function (err, res) {
+
+    if (err) console.log(err)
+    console.log(res)
+  });
+  res.send("Updated successfully!")
+})
