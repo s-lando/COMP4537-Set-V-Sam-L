8e4da49a147adb06d9f46006c04c836bfc09fd6d
s-lando <slandolando@hotmail.com>
2022-11-07 16:35:03 -0800

    added override function

---
+++ a1/errorOverride.js
@@ -0,0 +1,15 @@
+import { pokemonError } from './errorHandler.js';
+
+const overrideError = (err, req, res, next) => {
+  if (err instanceof pokemonError) {
+    res.status(err.statusCode);
+    res.send(err.message);
+  }
+  else {
+    res.status(500);
+    res.send("Internal Server Error");
+  }
+}
+
+export default { overrideError };
+
