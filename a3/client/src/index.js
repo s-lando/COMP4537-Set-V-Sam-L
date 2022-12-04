e800cb5ed5a64c94b5a2d5e4d7e466982d959fcf
s-lando <slandolando@hotmail.com>
2022-12-03 21:17:33 -0800

    fixed router bug

--- a3/client/src/index.js
+++ a3/client/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <h1>
+    <BrowserRouter>
       <Login />
-    </Router>
+    </BrowserRouter>
+    </h1>
   </React.StrictMode>
 );
 
